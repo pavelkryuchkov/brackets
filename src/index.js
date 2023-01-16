030c2782bb5f12f37356310db99d013927b17ff3
Pavel Kryuchkov <pavkr92@gmail.com>
2023-01-16 04:57:46 +0300

    feat: implement check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let opening = [];
+  let closing = [];
+  let bracketMap = {};
+  for (let [open, close] of bracketsConfig) {
+    opening.push(open);
+    closing.push(close);
+    bracketMap[close] = open;
+  }
+
+  let stack = [];
+  for (let bracket of str) {
+    if (opening.includes(bracket) && !closing.includes(bracket)) {
+      stack.push(bracket);
+    } else if (opening.includes(bracket) && closing.includes(bracket)) {
+      if (stack[stack.length - 1] === bracket) stack.pop();
+      else stack.push(bracket);
+    } else if (stack.length === 0 || stack.pop() !== bracketMap[bracket]) {
+      return false;
+    }
+  }
+  return stack.length === 0;
+};
